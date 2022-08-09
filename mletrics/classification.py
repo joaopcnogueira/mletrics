b9c0d660591451a0681b53b213ba79ab464e6739
João Nogueira <joao.nogueira@datarisk.io>
2022-08-08 21:17:48 -0300

    ks function added

--- mletrics/classification.py
+++ mletrics/classification.py
@@ -4,12 +4,18 @@
 __all__ = ['ks']
 
 # %% ../nbs/02_classification.ipynb 3
-import numpy as np
 import pandas as pd
-from numpy import ndarray
+from scipy.stats import ks_2samp
 
-def ks(y_proba0: ndarray,  # array of probabilities for class 0
-       y_proba1: ndarray): # array of probabilities for class 1
-    "Calculate the KS Metrics"
+def ks(y_true,    # the target variable, contains 0's and 1's
+       y_prob):   # the probability of the
 
-    pass
+   df = pd.DataFrame({
+      'target': y_true,
+      'probability': y_prob
+   })
+
+   class0 = df[df['target'] == 0]
+   class1 = df[df['target'] == 1]
+
+   return ks_2samp(class0['probability'], class1['probability']).statistic
